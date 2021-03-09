d3a8a9fbcf323d4f4004accc344913308315dcb1
Mayukh Sarkar <25590818+Mayukh-Sarkar@users.noreply.github.com>
2021-03-09 10:41:22 +0100

    final

--- Ch_2_time_response.m
+++ Ch_2_time_response.m
@@ -8,7 +8,7 @@ v_g = randn(1,N)/sqrt(dt);
 w_g = randn(1,N)/sqrt(dt);
 
 % INPUT VECTORS
-u1 = [nn' nn' u_g' nn'  nn'];     
+%u1 = [nn' nn' nn' nn'  nn'];     
 u2 = [nn' nn' nn'  v_g' nn'];
 u3 = [nn' nn' nn'  nn'  w_g'];
 
@@ -33,22 +33,22 @@ Dr1 = [0 0 0 0 0;
      0 0 0 0 0;
      0 0 0 0 0];
 % RESPONSE to u_g
-y1 = lsim(A2,B,C1,D,u1,t);
+%y1 = lsim(A2,B,C1,D,u1,t);
 % RESPONSE to v_g
 y2 = lsim(A2,B,C1,D,u2,t);
 % RESPONSE to w_g
 y3 = lsim(A2,B,C1,D,u3,t);
 % RESPONSE to all together (linear system!)
-yt = y1+y2+y3;
+yt = y2+y3;
 %xt = x1+x2+x3;
 a_y =  yt(:,5);
  
-y1r = lsim(Ar,Br,Cr1,Dr1,u1,t);
+%y1r = lsim(Ar,Br,Cr1,Dr1,u1,t);
 % RESPONSE to v_g
 y2r = lsim(Ar,Br,Cr1,Dr1,u2,t);
 % RESPONSE to w_g
 y3r = lsim(Ar,Br,Cr1,Dr1,u3,t);
-ytr = y1r+y2r+y3r;
+ytr = y2r+y3r;
 a_y_r = ytr(:,3);
 
 % PLOT RESULTS
